0807d3a79d86dd4a498f41c131eb0677e2eaab9a
BenBandarker <ben.bandarker@gmail.com>
2024-12-18 18:30:21 +0200

    18.12.24 update

--- rapidAPI/src/controllers/tournamentController.js
+++ rapidAPI/src/controllers/tournamentController.js
@@ -4,7 +4,20 @@ async function importTournaments(req, res) {
   try {
     const { country } = req.params;
     const tournaments = await fetchData('apiOne',`v3/leagues?country=${country}`);
-    // Code to save tournaments to the database using SQL
+    // Code to save tournaments to the database using SQL - DONE
+    for(const tournament of tournaments){
+      // Save tournament to the database
+      const params = [tournament.league.id,
+        tournament.league.name,
+        tournament.seasons[seasons.length-1].start,
+        tournament.seasons[seasons.length-1].end,
+        tournament.seasons[seasons.length-1].year,
+        tournament.country.name,
+      ];
+      const insertQuery = `INSERT INTO Tournaments (tournament_api_id, tournament_name, start_date, end_date, year, location) VALUES (?, ?, ?, ?, ?, ?)`;
+
+      await executeQuery(insertQuery, params);
+    }
     res.status(201).send('Tournaments imported successfully');
   } catch (error) {
     res.status(500).send('Error importing tournaments');
